bf9a5a0df290d9d62f94c8c20fac2c6b36f4d7d8
Muhammad-Jay <jsync4172004@gmail.com>
2025-06-24 12:07:47 +0100

    ui & components api route

--- app/api/components/[slug]/route.ts
+++ app/api/components/[slug]/route.ts
@@ -1,4 +1,5 @@
 import { NextRequest, NextResponse } from 'next/server'
+import {createClient} from "@/utils/supabase/client";
 
 export async function GET(
     req: NextRequest,
@@ -7,9 +8,13 @@ export async function GET(
     const { slug } = params
 
     try {
+        const supabase = await createClient()
+        const {data, error} = await supabase.from('components_slug').select().eq('slug', slug)
+        if (error){
+            return NextResponse.json({error})
+        }
 
-
-        return NextResponse.json({})
+        return NextResponse.json(data[0])
     } catch (err) {
         console.error(err)
         return NextResponse.json({ error: 'Internal server error' }, { status: 500 })
